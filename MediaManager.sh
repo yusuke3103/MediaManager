352519fe36f7c12f34eddbebfabd95a8919effa5
Yusuke Sato <>
2019-01-03 23:45:26 +0900

    起動スクリプット

---
+++ MediaManager.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd `dirname $0`
+CURRENT_DIR=`pwd`
+command=$1
+
+run(){
+	# eval ”. mmvenv/bin/activate”
+	eval ". mmvenv/bin/activate"
+	eval "nohup python mm/manage.py runserver > /dev/null 2>&1 < /dev/null &"
+	echo "RUN"
+}
+
+stop(){
+	pids=(`ps -ef | grep runserver | awk '{print $2;}'`)
+
+	kill -9 ${pids[1]}
+
+	echo "STOP"
+}
+
+
+if [ $command = "run" ]; then
+	run
+else
+	stop
+fi
+
